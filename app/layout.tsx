766de680e46d200b9cfeff3df33eb0e4b87b3faf
jinampanc-pixel <jinampanc@gmail.com>
2026-08-27 00:01:19 +0530

    Mount final managed label dropdown layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ import "./order-setup-polish.css";
 import "./order-compact-ux.css";
 import "./brand-header-final.css";
 import "./label-flow-polish.css";
+import "./label-dropdown-final.css";
 import { OwnerDropdownUx } from "./owner-dropdown-ux";
 import { OrderSetupPolish } from "./order-setup-polish";
 import { OrderSetupFinalize } from "./order-setup-finalize";
@@ -29,6 +30,7 @@ import { LabelFlowPolish } from "./label-flow-polish";
 import { LabelDesignerPolish } from "./label-designer-polish";
 import { LabelProductionReady } from "./label-production-ready";
 import { LabelFinalization } from "./label-finalization";
+import { LabelDropdownFinal } from "./label-dropdown-final";
 
 export const metadata: Metadata = {
   title: "Jinam Foundation",
@@ -40,5 +42,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en"><body><ErpOrderSync/><OwnerDropdownUx/><OrderSetupPolish/><OrderSetupFinalize/><OrderCompactUx/><WorkspaceTopPager/><WorkspaceShortcuts/><LabelFlowPolish/><LabelDesignerPolish/><LabelProductionReady/><LabelFinalization/>{children}</body></html>;
+  return <html lang="en"><body><ErpOrderSync/><OwnerDropdownUx/><OrderSetupPolish/><OrderSetupFinalize/><OrderCompactUx/><WorkspaceTopPager/><WorkspaceShortcuts/><LabelFlowPolish/><LabelDesignerPolish/><LabelProductionReady/><LabelFinalization/><LabelDropdownFinal/>{children}</body></html>;
 }
