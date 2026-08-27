3fa19c57dadb042c7314686bce7dc32915a071b5
jinampanc-pixel <jinampanc@gmail.com>
2026-08-27 11:18:39 +0530

    Mount focused label interactions

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ import { LabelFlowPolish } from "./label-flow-polish";
 import { LabelDesignerPolish } from "./label-designer-polish";
 import { LabelProductionReady } from "./label-production-ready";
 import { LabelFinalization } from "./label-finalization";
+import { LabelDesignerInteractions } from "./label-designer-interactions";
 
 export const metadata: Metadata = {
   title: "Jinam Foundation",
@@ -41,5 +42,5 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en"><body><ErpOrderSync/><OwnerDropdownUx/><OrderSetupPolish/><OrderSetupFinalize/><OrderCompactUx/><WorkspaceTopPager/><WorkspaceShortcuts/><LabelFlowPolish/><LabelDesignerPolish/><LabelProductionReady/><LabelFinalization/>{children}</body></html>;
+  return <html lang="en"><body><ErpOrderSync/><OwnerDropdownUx/><OrderSetupPolish/><OrderSetupFinalize/><OrderCompactUx/><WorkspaceTopPager/><WorkspaceShortcuts/><LabelFlowPolish/><LabelDesignerPolish/><LabelProductionReady/><LabelFinalization/><LabelDesignerInteractions/>{children}</body></html>;
 }
