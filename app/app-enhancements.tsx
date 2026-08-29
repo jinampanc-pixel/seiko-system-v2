d58f5a72132995feb7ab6db8ea91d2ead24a2b36
jinampanc-pixel <jinampanc@gmail.com>
2026-08-29 21:56:45 +0530

    Add Jinam identity to SEIKO shell

--- app/app-enhancements.tsx
+++ app/app-enhancements.tsx
@@ -13,6 +13,7 @@ import { LabelProductionReady } from "./label-production-ready";
 import { LabelFinalization } from "./label-finalization";
 import { LabelDesignerInteractions } from "./label-designer-interactions";
 import { GlobalNavigation } from "./global-navigation";
+import { JinamLegacyBrand } from "./jinam-legacy-brand";
 
 /**
  * Compatibility/enhancement adapters for the legacy SEIKO-root application.
@@ -34,5 +35,6 @@ export function AppEnhancements() {
     <LabelFinalization />
     <LabelDesignerInteractions />
     <GlobalNavigation />
+    <JinamLegacyBrand />
   </>;
 }
