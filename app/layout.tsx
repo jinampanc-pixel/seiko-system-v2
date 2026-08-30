76db97f93e87d316f50cc916b165db03e3169bda
jinampanc-pixel <jinampanc@gmail.com>
2026-08-31 03:35:52 +0530

    Load final SEIKO workspace and label UX styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,7 @@ import "./phase1-visual.css";
 import "./seiko-phase1.css";
 import "./seiko-phase2.css";
 import "./seiko-stage2-safe.css";
+import "./seiko-workspace-label-final.css";
 import { AccessProvider } from "./access-control";
 import { BusinessApplicationRouter } from "./business-application-router";
 
