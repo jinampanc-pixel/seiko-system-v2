b970d0a254f9a049bc34057ba3d0ef87c1a4a8c6
jinampanc-pixel <jinampanc@gmail.com>
2026-08-31 00:54:11 +0530

    Load SEIKO operational finish after legacy styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ import "./veyn-functional.css";
 import "./phase1-visual.css";
 import "./seiko-phase1.css";
 import "./seiko-phase2.css";
+import "./seiko-operational-v2.css";
 import { AccessProvider } from "./access-control";
 import { BusinessApplicationRouter } from "./business-application-router";
 
