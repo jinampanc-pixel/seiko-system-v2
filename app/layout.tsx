0ae9501c982e746b4216d38a772238ddbdd3a75f
jinampanc-pixel <jinampanc@gmail.com>
2026-08-30 14:50:05 +0530

    Load MeTh commerce settings styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ import "./access-control.css";
 import "./modern-auth.css";
 import "./jinam-shell.css";
 import "./channel-connections.css";
+import "./meth-commerce-settings.css";
 import "./veyn-app.css";
 import "./veyn-functional.css";
 import "./phase1-visual.css";
