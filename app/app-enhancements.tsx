15754ded1a18506aad5f026bc8f6637ade4f6f36
jinampanc-pixel <jinampanc@gmail.com>
2026-08-31 15:07:05 +0530

    Use compact SEIKO workspace row menu

--- app/app-enhancements.tsx
+++ app/app-enhancements.tsx
@@ -8,13 +8,14 @@ import { OrderSetupFinalize } from "./order-setup-finalize";
 import { OrderCompactUx } from "./order-compact-ux";
 import { WorkspaceTopPager } from "./workspace-top-pager";
 import { WorkspaceShortcuts } from "./workspace-shortcuts";
-import { WorkspaceRowActions } from "./workspace-row-actions";
+import { WorkspaceRowMenu } from "./workspace-row-menu";
 import { SeikoCloseConfirm } from "./seiko-close-confirm";
 import { LabelFlowPolish } from "./label-flow-polish";
 import { LabelDesignerPolish } from "./label-designer-polish";
 import { LabelProductionReady } from "./label-production-ready";
 import { LabelFinalization } from "./label-finalization";
 import { LabelDesignerInteractions } from "./label-designer-interactions";
+import { LabelWorkspaceRefinement } from "./label-workspace-refinement";
 import { GlobalNavigation } from "./global-navigation";
 import { SeikoPhase1 } from "./seiko-phase1";
 import { SeikoPhase2Gate } from "./seiko-phase2-gate";
@@ -33,13 +34,14 @@ export function AppEnhancements() {
     <OrderCompactUx />
     <WorkspaceTopPager />
     <WorkspaceShortcuts />
-    <WorkspaceRowActions />
+    <WorkspaceRowMenu />
     <SeikoCloseConfirm />
     <LabelFlowPolish />
     <LabelDesignerPolish />
     <LabelProductionReady />
     <LabelFinalization />
     <LabelDesignerInteractions />
+    <LabelWorkspaceRefinement />
     <GlobalNavigation />
     <SeikoPhase1 />
     <SeikoLibraryAutofill />
