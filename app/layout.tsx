f89984ab78eb5da981bc7c3df13caf1892447d2c
jinampanc-pixel <jinampanc@gmail.com>
2026-09-04 03:52:36 +0530

    Load final SEIKO review CSS last

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ import "./seiko-interface-fixes.css";
 import "./packing-workspace-audit.css";
 import "./packing-workflow-final.css";
 import "./seiko-final-ux-pass.css";
-import "./seiko-review-stabilization.css";
+import "./seiko-review-final.css";
 import { AccessProvider } from "./access-control";
 import { BusinessApplicationRouter } from "./business-application-router";
 
