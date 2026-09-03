86b9e81f1b85f810a833573e1d11240c8e01c8b2
jinampanc-pixel <jinampanc@gmail.com>
2026-09-03 11:07:41 +0530

    Let calibrated label print own Print clicks

--- app/app-enhancements.tsx
+++ app/app-enhancements.tsx
@@ -43,8 +43,8 @@ export function AppEnhancements() {
     <LabelFlowPolish />
     <LabelDesignerPolish />
     <LabelProductionReady />
-    <SeikoInterfaceFixes />
     <LabelFinalization />
+    <SeikoInterfaceFixes />
     <LabelDesignerInteractions />
     <LabelWorkspaceRefinement />
     <GlobalNavigation />
